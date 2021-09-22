f29df6fc06bb7b6c02373923c30f2ce52cef34c7
Elie Pernet <elie.pernet@orange.fr>
2021-09-22 20:37:25 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,187 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Btn component</h1>
+
+  <div class="section">
+    <div class="btn-section">
+      <p class="btn-title">{{ '<Button />' }}</p>
+      <CustomBtn text="Default" variant="default"/>
+    </div>
+
+    <div class="btn-section">
+      <p class="btn-title">&:hover, &:focus</p>
+      <CustomBtn text="Default" variant="default" hover="true"/>
+    </div>
+  </div>
+
+  <div class="section">
+    <div class="btn-section">
+      <p class="btn-title">{{ '<Button variant=”outline” />' }}</p>
+      <CustomBtn text="Default" variant="outlined"/>
+    </div>
+
+    <div class="btn-section">
+      <p class="btn-title">&:hover, &:focus</p>
+      <CustomBtn text="Default" variant="outlined" hover="true"/>
+    </div>
+  </div>
+
+  <div class="section">
+    <div class="btn-section">
+      <p class="btn-title">{{ '<Button variant=”text” />' }}</p>
+      <CustomBtn text="Default" variant="text"/>
+    </div>
+
+    <div class="btn-section">
+      <p class="btn-title">&:hover, &:focus</p>
+      <CustomBtn text="Default" variant="text" hover="true"/>
+    </div>
+  </div>
+
+  <div class="section">
+    <div class="btn-section">
+      <p class="btn-title">{{ '<Button disableShadow />' }}</p>
+      <CustomBtn text="Default" variant="default" disable-shadow="true"/>
+    </div>
+  </div>
+
+  <div class="section">
+    <div class="btn-section">
+      <p class="btn-title">{{ '<Button disabled />' }}</p>
+      <CustomBtn text="Disabled" disabled="true"/>
+    </div>
+
+    <div class="btn-section">
+      <p class="btn-title">{{ '<Button variant=”text” disabled />' }}</p>
+      <CustomBtn text="Disabled" variant="text" disabled="true"/>
+    </div>
+  </div>
+
+  <div class="section">
+    <div class="btn-section">
+      <p class="btn-title">{{ '<Button startIcon=”local_grocery_store” />' }}</p>
+      <CustomBtn text="Default" start-icon="local_grocery_store" color="primary"/>
+    </div>
+
+    <div class="btn-section">
+      <p class="btn-title">{{ '<Button endIcon=”local_grocery_store” />' }}</p>
+      <CustomBtn text="Default" end-icon="local_grocery_store" color="primary"/>
+    </div>
+  </div>
+
+  <div class="section">
+    <div class="btn-section">
+      <p class="btn-title">{{ '<Button size=”sm” />' }}</p>
+      <CustomBtn text="Default" size="sm" color="primary"/>
+    </div>
+
+    <div class="btn-section">
+      <p class="btn-title">{{ '<Button size=”md” />' }}</p>
+      <CustomBtn text="Default" size="md" color="primary"/>
+    </div>
+
+    <div class="btn-section">
+      <p class="btn-title">{{ '<Button size=”lg” />' }}</p>
+      <CustomBtn text="Default" size="lg" color="primary"/>
+    </div>
+  </div>
+
+  <div class="section">
+    <div class="btn-section">
+      <p class="btn-title">{{ '<Button color=”default” />' }}</p>
+      <CustomBtn text="Default"/>
+    </div>
+
+    <div class="btn-section">
+      <p class="btn-title">{{ '<Button color=”primary” />' }}</p>
+      <CustomBtn text="Default" color="primary"/>
+    </div>
+
+    <div class="btn-section">
+      <p class="btn-title">{{ '<Button color=”secondary” />' }}</p>
+      <CustomBtn text="Secundary" color="secondary"/>
+    </div>
+
+    <div class="btn-section">
+      <p class="btn-title">{{ '<Button color=”danger” />' }}</p>
+      <CustomBtn text="Danger" color="danger"/>
+    </div>
+  </div>
+
+  <div class="section">
+    <p class="btn-title">&:hover, &:focus</p>
+    <div>
+      <div class="btn-section">
+        <CustomBtn text="Default" variant="default" hover="true"/>
+      </div>
+
+      <div class="btn-section">
+        <CustomBtn text="Default" color="primary" hover="true"/>
+      </div>
+
+      <div class="btn-section">
+        <CustomBtn text="Secundary" color="secondary" hover="true"/>
+      </div>
+
+      <div class="btn-section">
+        <CustomBtn text="Danger" color="danger" hover="true"/>
+      </div>
+  </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import CustomBtn from "./components/CustomBtn";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { CustomBtn }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import './assets/reset.css';
+
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu+Mono&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500&display=swap');
+
+$primary-color: #333333;
+$secundary-color: #4F4F4F;
+
+
+body {
+  padding-left: 25px;
+  padding-top: 25px;
+
+  h1 {
+    font-family: 'Poppins', sans-serif;
+    color: $secundary-color;
+    font-size: 24px;
+    margin-bottom: 28px;
+  }
+}
+
+.section {
+  display: flex;
+
+  .btn-section {
+    margin-right: 75px;
+    margin-bottom: 45px;
+
+    .btn-title {
+      font-family: 'Ubuntu Mono', sans-serif;
+      margin-bottom: 12px;
+    }
+
+  }
+
+  &:last-child {
+    display: flex;
+    flex-direction: column;
+
+    p { margin-bottom: 12px; }
+    div { display: flex; }
+  }
 }
 </style>
---
+++ src/components/CustomBtn.vue
@@ -0,0 +1,188 @@
+<template>
+  <button class="btn" :class="{
+    'btn--hover': hover && variant === 'default',
+    'btn--sm': size === 'sm',
+    'btn--md': size === 'md',
+    'btn--lg': size === 'lg',
+    'btn--default': color === 'default',
+    'btn--primary': color === 'primary',
+    'btn--secondary': color === 'secondary',
+    'btn--danger': color === 'danger',
+    'btn--default--hover': color === 'default' && hover,
+    'btn--primary--hover': color === 'primary' && hover,
+    'btn--secondary--hover': color === 'secondary' && hover,
+    'btn--danger--hover': color === 'danger' && hover === 'true',
+    'btn--outlined': variant === 'outlined',
+    'btn--outlined--hover': hover && variant === 'outlined' && !disabled,
+    'btn--text': variant === 'text',
+    'btn--text--hover': hover && variant === 'text' && !disabled,
+    'btn--text--disabled': disabled && variant === 'text',
+    'btn--disableShadow': disableShadow,
+    'btn--disabled': disabled,
+  }">
+    <span v-show="startIcon" class="material-icons-outlined icon"> {{ startIcon }}</span>
+    {{ text }}
+    <span v-show="endIcon" class="material-icons-outlined icon">{{ endIcon }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    'text': {
+      type: String,
+      default: 'Default'
+    },
+    'variant': {
+      type: String,
+      default: 'default'
+    },
+    'hover': {
+      type: Boolean,
+      default: false
+    },
+    'disableShadow': {
+      type: Boolean,
+      default: false
+    },
+    'disabled': {
+      type: Boolean,
+      default: false
+    },
+    'startIcon': String,
+    'endIcon': String,
+    'size': {
+      type: String,
+      default: 'md'
+    },
+    'color': {
+      type: String,
+      default: 'default'
+    }},
+}
+
+</script>
+
+<style lang="scss" scoped>
+$primary: #2962FF;
+$secondary: #455A64;
+$danger: #D32F2F;
+
+.btn {
+  display: flex;
+  align-items: center;
+
+  border-radius: 6px;
+
+  font-family: 'Noto Sans JP', sans-serif;
+
+  &--sm {
+    padding: 6px 12px;
+  }
+
+  &--md {
+    padding: 8px 16px;
+  }
+
+  &--lg {
+    padding: 11px 22px;
+  }
+
+  &--default {
+    background: #E0E0E0;
+    box-shadow: 0 2px 3px rgba(51, 51, 51, 0.2);
+    color: #3F3F3F;
+
+    &--hover {
+      background: #AEAEAE;
+    }
+  }
+
+  &--primary {
+    background: $primary;
+    color: #ffffff;
+    box-shadow: 0 2px 3px rgba(41, 98, 255, 0.2);
+
+    &--hover {
+      background: #0039CB;
+      box-shadow: 0px 2px 3px rgba(41, 98, 255, 0.2);
+    }
+  }
+
+  &--secondary {
+    background: $secondary;
+    color: #ffffff;
+
+    &--hover {
+      background: #1C313A;
+    }
+  }
+
+  &--danger {
+    background: $danger;
+    color: #ffffff;
+    box-shadow: 0 2px 3px rgba(211, 47, 47, 0.2);
+
+    &--hover {
+      background: #9A0007;
+    }
+  }
+
+  span:first-child {
+    margin-right: 10px;
+  }
+
+  span:last-child {
+    margin-left: 10px;
+  }
+
+  .icon {
+    font-size: 19px;
+  }
+
+  &--outlined {
+    border: 1px solid #3D5AFE;
+    color: #3D5AFE;
+    background: #ffffff;
+    box-shadow: none;
+
+    &--hover {
+      background: rgba(41, 98, 255, 0.1);
+      box-shadow: none;
+    }
+  }
+
+  &--text {
+    border: none;
+    background: none;
+    box-shadow: none;
+
+    color: #3D5AFE;
+
+    padding: 8px 0;
+
+    &--hover {
+      padding: 8px 16px;
+      background: rgba(41, 98, 255, 0.1);
+      border-radius: 6px;
+      box-shadow: none;
+      color: #3D5AFE;
+    }
+
+    &--disabled {
+      color: #9E9E9E !important;
+      background: #ffffff !important;
+      padding: 8px 0 !important;
+    }
+  }
+
+  &--disableShadow {
+    box-shadow: none;
+  }
+
+  &--disabled {
+    background: #E0E0E0;
+    color: #9E9E9E;
+  }
+}
+</style>
